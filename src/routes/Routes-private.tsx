f5685b759cb7e9930342dac1e4c0152f0496f219
anabsena <anabsenaf@gmail.com>
2024-09-18 14:12:16 -0300

    organized pastes

--- src/routes/Routes-private.tsx
+++ src/routes/Routes-private.tsx
@@ -1,13 +1,13 @@
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import App from '../App';
+import Home from '../Screens/Home/Home';
 
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
